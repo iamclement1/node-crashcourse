700a454d521278c634a409a7b18fb28d538b2685
Nnamdi Azubuike <35131360+iamclement1@users.noreply.github.com>
2023-06-28 11:50:33 +0100

    "feats:  reading and printng objects"

--- test.js
+++ test.js
@@ -6,3 +6,12 @@ const greet = (name) => {
 }
 greet("clement");
 greet("Winnie");
+
+//global object
+setTimeout(() => {
+   console.log("first object");
+}, 3000);
+
+const int = setInterval(() => {
+   console.log("in the interval");
+}, 1000);
